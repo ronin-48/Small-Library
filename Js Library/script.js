e608e0a5d47a45864f99515293d1fad5614c7df3
abdel rahmen <52658187+ronin-48@users.noreply.github.com>
2023-01-14 21:40:24 +0100

    Add files via upload

---
+++ Js Library/script.js
@@ -0,0 +1,293 @@
+//welcome my friends to my libreay in js
+
+//bismiallah 
+
+/******************** - Note - ******************************/
+// S capital == Start & E capital == End
+
+/***********************************************************/
+
+/******************** - S-output - ******************************/
+ 
+//  Writing into an HTML element, using ---innerHTML.---
+// Writing into the HTML output using ---document.write().---
+// Writing into an alert box, using ---window.alert().---
+// Writing into the browser console, using ---console.log().---
+
+/******************** - E-output - ******************************/
+
+/******************** - S-variables - ******************************/
+
+// 	4 Ways to Declare a JavaScript Variable:
+//	 Using --var--
+//	 Using --let--
+// 	 Using --const--
+// 	 Using --nothing--
+
+/******************** - E-variables - ******************************/
+
+/******************** - S-Opreator - ******************************/
+
+// 	+	Addition 						
+// 	-	Subtraction
+// 	*	Multiplication
+// 	**	Exponentiation (ES2016)
+// 	/	Division
+// 	%	Modulus (Division Remainder)
+// 	++	Increment
+// 	--	Decrement
+
+
+
+
+// 	=		x = y		x = y
+// 	+=		x += y		x = x + y
+// 	-=		x -= y		x = x - y
+// 	*=		x *= y		x = x * y
+// 	/=		x /= y		x = x / y
+// 	%=		x %= y		x = x % y
+//	**=		x **= y		x = x ** y
+
+/******************** - E-Opreator - ******************************/
+
+/******************** - S-Data type - ******************************/
+
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// The Object Datatype
+// The object data type can contain:
+
+// 1. An object
+// 2. An array
+// 3. A date
+
+/******************** - E-Data type - ******************************/
+
+/******************** - S-Fucnction  - ******************************/
+
+// function name(parameter1, parameter2, parameter3) {
+  // code to be executed
+// }
+
+/******************** - E-Fucnction  - ******************************/
+
+/******************** - S-Object  - ******************************/
+
+// const person = {
+//   firstName: "Abou",
+//   lastName: "Bm",
+//   age: 17,
+//   eyeColor: "Brown"
+// }; 
+
+// 	Note:
+// You can access object properties in two ways:
+
+			// objectName.propertyName
+
+/******************** - E-Object  - ******************************/
+
+/******************** - S-Evant - ********************************/
+
+
+// ---onchange---		An HTML element has been changed
+// ---onclick---		The user clicks an HTML element
+// ---onmouseover---	The user moves the mouse over an HTML element
+// ---onmouseout---		The user moves the mouse away from an HTML element
+// ---onkeydown---		The user pushes a keyboard key
+// ---onload---			The browser has finished loading the page
+
+/******************** - E-Evant - ********************************/
+
+/******************** - S-String method - ********************************/
+
+/******************** - E-String method - ********************************/
+
+/******************** - S-Array - ********************************/
+
+// let Bro1 = "Abdou";	-- (x)
+// let Bro2 = "Amin";	-- (x)
+// let Bro3 = "Imad";	-- (x)
+
+// use  
+
+// const Feiends = ["Abdou", "Amin", "Imad"];   
+
+// const Feiends = [
+//   "Abdou",
+//   "Amin",
+//   "Imad"
+// ];
+
+/******************** - E-Array - ********************************/
+
+/******************** - S-Array method - ********************************/
+
+// The --push()-- method adds a new element to an array (at the end):
+// The --unshift()-- method adds a new element to an array (at the beginning), and "unshifts" older elements:
+
+/******************** - E-Array method- ********************************/
+
+/******************** - S-Math - ********************************/
+
+// Math.E        	// returns Euler's number
+// Math.PI       	// returns PI
+// Math.SQRT2    	// returns the square root of 2
+// Math.SQRT1_2  	// returns the square root of 1/2
+// Math.LN2      	// returns the natural logarithm of 2
+// Math.LN10     	// returns the natural logarithm of 10
+// Math.LOG2E    	// returns base 2 logarithm of E
+// Math.LOG10E  	// returns base 10 logarithm of E
+// Math.random();	//returns random num
+
+
+/******************** - E-Math - ********************************/
+
+/******************** - S-Boolean - ********************************/
+
+// 		==		equal to	
+// 		>		greater than	
+// 		<		less than
+// 		===		equal value and equal type		
+// 		!=		not equal		
+// 		!==		not equal value or not equal type		
+// 		>		greater than	
+// 		<		less than		
+// 		>=		greater than or equal to		
+// 		<=		less than or equal to
+/******************** - E-Boolean - ********************************/
+
+/******************** - S-If else - ********************************/
+
+// Use ---if--- to specify a block of code to be executed, if a specified condition is true
+// Use ---else--- to specify a block of code to be executed, if the same condition is false
+// Use ---else--- if to specify a new condition to test, if the first condition is false
+
+
+// if (condition) {
+		//  block of code to be executed if the condition is true
+// }
+
+
+/******************** - E-If else - ********************************/
+
+/******************** - S-Switch  - ********************************/
+
+// Use ---switch--- to specify many alternative blocks of code to be executed
+
+// switch(expression) {
+//   case x:
+//     // code block
+//     break;
+//   case y:
+//     // code block
+//     break;
+//   default:
+//     // code block
+// }
+
+
+/******************** - E-Switch  - ********************************/
+
+/******************** - S-For loop  - ********************************/
+
+// for - loops through a block of code a number of times
+// for/in - loops through the properties of an object
+// for/of - loops through the values of an iterable object
+// while - loops through a block of code while a specified condition is true
+// do/while - also loops through a block of code while a specified condition is true 
+
+// for (let i = 0; i < cars.length; i++) {
+//   text += cars[i] + "<br>";
+// }
+// -----------------------
+
+// for (variable in array) {
+//   code
+// }
+// ----------------------
+
+// for (variable of iterable) {
+//   // code block to be executed
+// }
+// ---------------------
+
+// do {
+//   // code block to be executed
+// }
+// while (condition);
+
+/******************** - E-For loop  - ********************************/
+
+
+/******************** - S- Classes  - ********************************/
+
+// class Friends {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.year = age;
+//   }
+// }
+
+/******************** - E- Classes  - ********************************/
+
+/******************** - E-Dom document  - ********************************/
+// document.getElementById(id)				Find an element by element id
+// document.getElementsByTagName(name)		Find elements by tag name
+// document.getElementsByClassName(name)	Find elements by class name
+// element.innerHTML =  new html content	Change the inner HTML of an element
+// element.attribute = new value			Change the attribute value of an HTML element
+// element.style.property = new style		Change the style of an HTML element
+// Method									Description
+// element.setAttribute(attribute, value)	Change the attribute value of an HTML element
+// document.createElement(element)			Create an HTML element
+// document.removeChild(element)			Remove an HTML element
+// document.appendChild(element)			Add an HTML element
+// document.replaceChild(new, old)			Replace an HTML element
+// document.write(text)						Write into the HTML output stream
+// document.anchors							Returns all <a> elements that have a name attribute	
+// document.applets							Deprecated	
+// document.baseURI							Returns the absolute base URI of the document	
+// document.body							Returns the <body> element	
+// document.cookie							Returns the document's cookie	
+// document.doctype							Returns the document's doctype	
+// document.documentElement					Returns the <html> element	
+// document.documentMode					Returns the mode used by the browser	
+// document.documentURI						Returns the URI of the document	
+// document.domain							Returns the domain name of the document server	
+// document.domConfig						Obsolete.	
+// document.embeds							Returns all <embed> elements	
+// document.forms							Returns all <form> elements	
+// document.head							Returns the <head> element	
+// document.images							Returns all <img> elements	
+// document.implementation					Returns the DOM implementation	
+// document.inputEncoding					Returns the document's encoding (character set)	
+// document.lastModified					Returns the date and time the document was updated	
+// document.links							Returns all <area> and <a> elements that have a href attribute	
+// document.readyState						Returns the (loading) status of the document	
+// document.referrer						Returns the URI of the referrer (the linking document)	
+// document.scripts							Returns all <script> elements	
+// document.strictErrorChecking				Returns if error checking is enforced	
+// document.title							Returns the <title> element	
+// document.URL								Returns the complete URL of the documen
+
+/******************** - E-Dom document  - ********************************/
+
+/******************** - S-Dom-css  - ********************************/
+
+// Syntax ==> document.getElementById(id).style.property = new style
+
+/******************** - E-Dom-css  - ********************************/
+/******************** - E-Evant  - ********************************/
+
+//Syntax ==> document.getElementById("myBtn").addEventListener("click", function(){ alert("Hello World!"); });
+
+/******************** - E-Evant  - ********************************/
